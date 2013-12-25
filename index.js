95f3bbc6ae32c62c955ada2b2dc1c1f2a8e996b6
deathcap <deathcap@gmx.co.uk>
2013-12-24 18:42:21 -0800

    Add getItemTexture and getItemPileTexture

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = function(game, opts) {
 };
 
 function Registry(game, opts) {
+  this.game = game;
+
   this.blockProps = [ {} ];
   this.blockName2ID = { air:0 };
 
@@ -18,11 +20,6 @@ Registry.prototype.registerBlock = function(name, props) {
 
   // default properties
   props.name = props.name || name;
-  if (props.texture && !props.itemTexture) {
-    // use top of block texture as item texture by default
-    props.itemTexture = typeof props.texture === 'string' ? props.texture : props.texture[0];
-  }
-
   this.blockProps.push(props);
   this.blockName2ID[name] = nextID;
 
@@ -62,6 +59,30 @@ Registry.prototype.registerItem = function(name, props) {
 
 Registry.prototype.getItemProps = function(name) {
   return this.itemProps[name] || this.getBlockProps(name); // blocks are implicitly also items
+};
+
+Registry.prototype.getItemTexture = function(name) {
+  var props = this.getItemProps(name);
+  var textureName = props.itemTexture;
+
+  if (textureName === undefined) {
+    if (props.texture !== undefined) {
+      // no item texture, use block texture
+      // TODO: 3D cube, maybe with CSS transforms, like https://github.com/shama/craft
+      textureName = typeof props.texture === 'string' ? props.texture : props.texture[0];
+    } else {
+      // TODO: default for missing texture
+      textureName = 'gravel';
+    }
+  }
+  // TODO: different image categories, blocks, items may be in different folders
+
+  // TODO: should this return a three.js image, instead of a URL? how about zipped or stitched texture packs?
+  return this.game.materials.texturePath + textureName + '.png';
+};
+
+Registry.prototype.getItemPileTexture = function(itemPile) {
+  return this.getItemTexture(itemPile.item);
 };
 
 // return true if this name is a block, false otherwise (may be an item)
